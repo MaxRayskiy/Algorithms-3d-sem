9094e90ba4523ee52960451fd35e7246667e92b9
Max <max.rayskiy@gmail.com>
2019-09-29 00:40:16 +0300

    asymptotic_correction

--- M1/A/main.cpp
+++ M1/A/main.cpp
@@ -1,4 +1,4 @@
-/*! @file main.cpp
+/*! @file
 Найдите все вхождения шаблона в строку. Длина шаблона – p, длина строки – n.
 p <= 30000, n <= 300000.
 
@@ -18,59 +18,52 @@ using std::endl;
 using std::string;
 using std::vector;
 
-/*!
- * @function SubstringSearch
- * @param pattern - шаблон
- * @param text - обрабатываемый текст
- * @param answers - вектор ответов
- */
-void SubstringSearch(string &pattern, string &text, vector<int> &answers);
-
-/*!
- * @function ZFunction
- * @param text - анализируемый текст
- * @param z_function - результат z-функции на этом тектсе
- */
-void ZFunction(string &text, vector<int> &z_function);
+void SubstringPositionByPrefixFunction(string &pattern, string &text, vector<int> &answers);
 
 int main() {
     string pattern, text;
     cin >> pattern >> text;
 
-    vector<int> answers; // сюда занесем ответы
-    SubstringSearch(pattern, text, answers);
+    vector<int> answers;
+    SubstringPositionByPrefixFunction(pattern, text, answers);
 
-    for (const auto answer: answers)
+    for (const int &answer: answers)
         cout << answer << " ";
     cout << endl;
 
     return 0;
 }
 
-void SubstringSearch(string &pattern, string &text, vector<int> &answers) {
-    string full_text = pattern + '#' + text;
-    vector<int> z_function_result(full_text.size());
-    ZFunction(full_text, z_function_result);
 
-    int pattern_len = pattern.length();
-    for (unsigned int i = pattern_len + 1; i < z_function_result.size(); ++i)
-        if (z_function_result[i] == pattern_len)
-            answers.push_back(i - pattern_len - 1);
-}
+void SubstringPositionByPrefixFunction(string &pattern, string &text, vector<int> &answers) {
+    unsigned int pattern_size = pattern.size();
 
-void ZFunction(string &text, vector<int> &z_function) {
-    int text_length = text.length();
+    vector<int> pattern_prefix_function(pattern_size);
+    pattern_prefix_function[0] = 0;
 
-    z_function[0] = text_length;
+    int previous_value = 0;
+    // pattern prefix function
+    for (unsigned int i = 1; i < pattern.size(); ++i) {
+        while (previous_value > 0 && pattern[i] != pattern[previous_value])
+            previous_value = pattern_prefix_function[previous_value - 1];
 
-    int left = 0, right = 0;
-    for (int i = 1; i < text_length; ++i) {
-        z_function[i] = std::max(0, std::min( right - i, z_function[i - left]));
-        while (i + z_function[i] < text_length && text[z_function[i] + i] == text[z_function[i]])
-            ++z_function[i];
-        if (i + z_function[i] > right) {
-            left = i;
-            right = i + z_function[i];
-        }
+        if (pattern[i] == pattern[previous_value])
+            previous_value++;
+
+        pattern_prefix_function[i] = previous_value;
+    }
+
+    int current_value = 0;
+    // (pattern + "#" + text) prefix function
+    for (unsigned int i = 0; i < text.size(); ++i) {
+        while (current_value > 0 && text[i] != pattern[current_value])
+            current_value = pattern_prefix_function[current_value - 1];
+
+        if (text[i] == pattern[current_value])
+            current_value++;
+
+        // if
+        if (current_value == pattern_size)
+            answers.push_back(i - pattern_size + 1);
     }
 }
